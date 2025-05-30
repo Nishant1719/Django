9885f6adc14951a5a491b4f66ac428cba1a04152
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-30 15:23:27 +0530

    Implement initial Django app structure with views, models, and admin setup; add home view rendering

--- Djangopractice/Djangopractice/views.py
+++ Djangopractice/Djangopractice/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def home(request):
     # return HttpResponse("Hello, World! Home page")
     return render(request, 'website/index.html')
