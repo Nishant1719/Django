acc35c22b902c3a6ee1e3b1bccb49d0faf148233
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-30 11:49:03 +0530

    Add static files and templates for website structure
    - Create a new CSS file for styling
    - Add index.html template with static file linking
    - Update settings.py to include static files directory
    - Modify views.py to render the index.html template

--- Djangopractice/Djangopractice/views.py
+++ Djangopractice/Djangopractice/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def home(request):
-    return HttpResponse("Hello, World! Home page")
+    # return HttpResponse("Hello, World! Home page")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("Hello, World! about page")
