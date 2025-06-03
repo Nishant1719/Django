b38c4ced4026ed4bb8acbe8d86751ec7dc5818c1
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-06-03 15:56:18 +0530

    Add django-browser-reload integration and update styles for improved UI

--- Djangopractice/Djangopractice/settings.py
+++ Djangopractice/Djangopractice/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'DjangoApp',
     'tailwind',
     'theme',
+    'django_browser_reload',
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -55,6 +56,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    "django_browser_reload.middleware.BrowserReloadMiddleware", # always keep it last
+    
 ]
 
 ROOT_URLCONF = 'Djangopractice.urls'
--- Djangopractice/Djangopractice/urls.py
+++ Djangopractice/Djangopractice/urls.py
@@ -24,4 +24,8 @@ urlpatterns = [
     path('about/',views.about,name="about"),
     path('contact/',views.contact,name="contact"),
     path('DjangoApp', include('DjangoApp.urls')),
+    
+    
+    # Keep it last
+    path("__reload__/",include("django_browser_reload.urls")),
 ]
