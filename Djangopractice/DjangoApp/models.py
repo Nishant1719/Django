ad2561ecd1d9441edde3e20c8bb2f9142d83c56d
Nishant1719 <nishant.waghade17@gmail.com>
2025-06-05 14:11:47 +0530

    Refactor chaiVarity model and update views for chai listing

--- Djangopractice/DjangoApp/models.py
+++ Djangopractice/DjangoApp/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 class chaiVarity(models.Model):
+    # Models can be completely modified here which refected on the admin page.
     CHAI_TYPE_CHOICE = [
         ('ML','masala'),
         ('GR','ginger'),
@@ -14,3 +15,8 @@ class chaiVarity(models.Model):
     image = models.ImageField(upload_to='chais/')
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
+    
+    def __str__(self):
+        return self.name
+    
+    
--- Djangopractice/DjangoApp/views.py
+++ Djangopractice/DjangoApp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import chaiVarity
 
 # Create your views here.
 
 def all_DjangoApp(request):
-    return render(request,'DjangoApp/all_DjangoApp.html')
+    chais = chaiVarity.objects.all()
+    return render(request,'DjangoApp/all_DjangoApp.html',{'chais':chais})
 
 
 def about_app(request):
     return render(request,'DjangoApp/about_app.html')
+
