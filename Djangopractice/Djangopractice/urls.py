3b5aaa63d4f6dc29e6e2239ab4becbccccb5199f
Nishant1719 <nishant.waghade17@gmail.com>
2025-06-06 14:21:50 +0530

    Add new image file 'images.webp' to the media directory for chai assets

--- Djangopractice/Djangopractice/urls.py
+++ Djangopractice/Djangopractice/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('about/',views.about,name="about"),
     path('contact/',views.contact,name="contact"),
-    path('DjangoApp', include('DjangoApp.urls')),
+    path('DjangoApp/', include('DjangoApp.urls')),
     
     
     # Keep it last
